ff37ac6c76251d8f3e8e38b2afa208a979521d7e
joe-chtipol <joe.chtipol@gmail.com>
2019-03-06 15:28:15 +0100

    suite

---
+++ src/main/java/fr/ych/userstory/banking/util/ConstantsUtils.java
@@ -0,0 +1,12 @@
+package fr.ych.userstory.banking.util;
+
+public class ConstantsUtils {
+	
+	
+	public static final String DEPOSIT_TYPE="D";
+	
+	public static final String WITHDRAW_TYPE="W";
+
+	
+
+}
